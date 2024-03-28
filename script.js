6033ad8a56aed798ffc8697a0d469f76b072e491
Mike M. <millerdev17@gmail.com>
2024-03-27 22:14:59 -0400

    v 1.0, fixeddecimal button logic

--- script.js
+++ script.js
@@ -46,9 +46,8 @@ function operate(operator, a, b){
 
 numberButtons.forEach((element)=>{
     element.addEventListener('click', ()=>{
-        if(result == 'e' && displayTop.textContent !== ''){
-            clear();
-        }
+        if(displayBottom.textContent == result && displayTop.textContent) clear();
+            
         //assigns value to secondNumber variable if the operator variable is NOT empty
         if(operator !== ''){
             secondNumber += element.textContent;
@@ -65,6 +64,13 @@ numberButtons.forEach((element)=>{
 
 operandButtons.forEach((element)=>{
     element.addEventListener('click',()=>{
+        if(firstNumber == '.') return;
+        if(firstNumber !== '' && result !== ''){
+            firstNumber = result;
+            result = '';
+            displayBottom.textContent = firstNumber;
+        }
+        if(firstNumber == '') return;// fix for negative numbers
         if(operator !== '' && secondNumber == '')return;
         if(operator !== ''){
             displayTop.textContent = `${firstNumber} ${operator} ${secondNumber}`
@@ -79,6 +85,20 @@ operandButtons.forEach((element)=>{
         displayBottom.textContent = displayValue;
     })
 })
+
+periodButton.addEventListener('click', () =>{
+    if(displayBottom.textContent == result && displayTop.textContent) clear();
+    if(secondNumber == ''){
+        if(firstNumber.toString().includes('.')) return;
+        firstNumber += periodButton.textContent;
+        displayBottom.textContent = firstNumber;
+        console.log(firstNumber);
+    }else{
+        if(secondNumber.toString().includes('.')) return;
+            secondNumber += periodButton.textContent;
+            displayBottom.textContent = `${firstNumber} ${operator} ${secondNumber}`
+        }
+    })
 
 
 deleteButton.addEventListener('click', ()=>{
@@ -98,160 +118,19 @@ deleteButton.addEventListener('click', ()=>{
 })
 
 
-
-
-
+//creates blank calculator 
 allClearButton.addEventListener('click', ()=>{
     clear();
 })
-
+//MAKES IT DO MATH
 equalButton.addEventListener('click',()=>{
     if(secondNumber == '') return;
     displayTop.textContent = displayValue;
     result = operate(operator,firstNumber,secondNumber);
-    firstNumber=result;
-    result='e';
+    displayBottom.textContent = result;
+    firstNumber = result;
     secondNumber='';
     operator='';
-    displayBottom.textContent = firstNumber;
-    
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-//values
-let input = "";
-let operator = "";
-let firstNumber ="";
-let secondNumber = "";
-let result = '';
-let placeholder = '';
-
-const reset = ()=>{
-    input = "";
-    operator = "";
-    firstNumber ="";
-    secondNumber = "";
-    result = '';
-}
-
-
-const operate = op =>{
-    if(op == '+') return +firstNumber + +secondNumber;
-    if(op == '−') return +firstNumber - +secondNumber;
-    if(op == '×') return +firstNumber * +secondNumber;
-    if(op == '÷') return +firstNumber / +secondNumber;
-}
-
-const equals = ()=>{
-    secondNumber = input;
-    input = '';
-    placeholder =`${firstNumber} ${operator} ${secondNumber}`;
-    displayTop.textContent = placeholder;
-    input = operate(operator);
-    displayBottom.textContent = input;
     
     
-}
-//add event listener to all number buttons
-numberButtons.forEach((element)=>{
-    element.addEventListener('click', ()=>{        
-        input += element.textContent;
-        displayBottom.textContent = `${firstNumber} ${operator} ${input}`;        
-    })    
 })
-
-//add event listener to operand buttons
-operandButtons.forEach((element)=>{
-    element.addEventListener('click', ()=>{
-        //checks to see if value is only a decimal with no integers 
-        if(input.toString().split('') == ('.')) return;
-        //checks to see if input had value
-        if(input == '') return;
-        if(operator !== ""){
-            equals();
-            secondNumber = '';
-            operator = '';
-        } 
-        
-
-        //assigning values to variables
-        operator = element.textContent;
-        firstNumber = input;
-        input = '';
-        secondNumber = input;
-        displayBottom.textContent = `${firstNumber} ${operator} ${input}`;
-
-        
-        
-    })
-})
-
-//check to which operand to use and solve.
-equalButton.addEventListener('click',()=>{
-    
-    equals();
-
-})
-
-
-//Clear all values, new calculator
-allClearButton.addEventListener('click', ()=>{
-    input = "";
-    operator = "";
-    firstNumber ="";
-    secondNumber = "";
-    displayBottom.textContent = '';
-    displayTop.textContent = '';
-})
-
-
-
-// adds decimal to input. checks to only allow 1 decimal point
-periodButton.addEventListener('click',()=>{
-    if(input.toString().split('') == ('.')) return;
-    input += periodButton.textContent;
-    displayBottom.textContent = input;
-})
-
-
-
-//deleted the last number of input value;
-deleteButton.addEventListener('click', ()=>{
-    
-    input = input.toString().split('').slice(0,-1).join('');
-    displayBottom.textContent = input;
-    
-})
-
-
-
-// stop equal from running the problem with every click
-// fix the delete button for when its more then the first number
-    // check if there is an operator and second number
-        // if 2nd number, run normal delete function.
-        // if operator has a value, reset the value to nothing and remove from display
-
-*/
